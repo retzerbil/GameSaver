1006bbcb5c13dcc121c9e54a9363a0c27b8d4494
Andreas Retzius <andreasretzius@gmail.com>
2024-12-05 13:42:56 +0100

    Code formatting

--- src/components/DealCard.tsx
+++ src/components/DealCard.tsx
@@ -2,44 +2,57 @@ import { Anchor, Badge, Button, Card, Group, Image, Text } from "@mantine/core";
 import { IGameResponse } from "../models/IGameResponse";
 
 interface IDealCardProps {
-    game: IGameResponse;
+	game: IGameResponse;
 }
 
 const calculateDiscount = (retailPrice: number, salePrice: number) => {
-    return Math.round(((retailPrice - salePrice) / retailPrice) * 100);
-}
-export const DealCard = ({game}: IDealCardProps) => {
+	return Math.round(((retailPrice - salePrice) / retailPrice) * 100);
+};
+export const DealCard = ({ game }: IDealCardProps) => {
+	return (
+		<Card shadow="sm" padding="lg" radius="md" withBorder>
+			<Card.Section>
+				{game.deal ? (
+					<>
+						<Image
+							radius="md"
+							h={200}
+							src={game.thumb}
+							w="100%"
+							alt={game.external + "box art"}
+							style={{ objectFit: "cover" }}
+						></Image>
 
-    return (
-        <Card shadow="sm" padding="lg" radius="md" withBorder>
-            <Card.Section>
-                {game.deal ? (
-                        <>  
-                            <Image 
-                                radius="md" 
-                                h={200} 
-                                src={game.thumb} 
-                                w="100%"
-                                alt={game.external + "box art"}
-                                style={{objectFit: 'cover'}}>
-                            </Image>
+						<Group justify="space-between" mt="md" mb="xs">
+							<Text>{game.external}</Text>
+							<Badge color="green">
+								{calculateDiscount(
+									+game.deal.gameInfo.retailPrice,
+									+game.cheapest
+								)}
+								%
+							</Badge>
+						</Group>
 
-                            <Group justify="space-between" mt="md" mb="xs">
-                            <Text>{game.external}</Text> 
-                            <Badge color="green">{calculateDiscount(+game.deal.gameInfo.retailPrice, +game.cheapest)}%</Badge>
-                            </Group>
+						<Group justify="space-between">
+							<Text td="line-through">${game.deal.gameInfo.retailPrice}</Text>
+							<Text>${game.cheapest}</Text>
+						</Group>
 
-                            <Group justify="space-between">
-                                <Text td="line-through">${game.deal.gameInfo.retailPrice}</Text>
-                                <Text>${game.cheapest}</Text>
-                            </Group>
-                            
-                            <Button color="orange" fullWidth mt="md" radius="md"><Anchor underline="never" href={`https://www.cheapshark.com/redirect?dealID=${game.cheapestDealID}`} target="_blank">Go to store!</Anchor></Button>
-                        </>
-                    ) : (
-                        <div>No deal available</div>
-                )}
-            </Card.Section>
-        </Card>
-    )
-}
+						<Button color="orange" fullWidth mt="md" radius="md">
+							<Anchor
+								underline="never"
+								href={`https://www.cheapshark.com/redirect?dealID=${game.cheapestDealID}`}
+								target="_blank"
+							>
+								Go to store!
+							</Anchor>
+						</Button>
+					</>
+				) : (
+					<div>No deal available</div>
+				)}
+			</Card.Section>
+		</Card>
+	);
+};
