909e58b74f64ceabc71478cb0e0ea6505f06b836
Andreas Retzius <andreasretzius@gmail.com>
2024-12-06 14:54:44 +0100

    More noticeable spinner for when loading

--- src/components/GameSearch.tsx
+++ src/components/GameSearch.tsx
@@ -7,6 +7,7 @@ import {
 	TextInput,
 	Loader,
 	Flex,
+	LoadingOverlay,
 } from "@mantine/core";
 import { useState, useEffect } from "react";
 import { DealCard } from "./DealCard";
@@ -100,67 +101,72 @@ export const GameSearch = () => {
 					Search!
 				</Button>
 			</form>
+			<div style={{ position: "relative" }}>
+				<LoadingOverlay
+					visible={isLoading}
+					zIndex={1000}
+					overlayProps={{ radius: "sm", blur: 2 }}
+				/>
 
-			{isLoading && <Loader color="orange" />}
-			{error && <p style={{ color: "red" }}>{error}</p>}
+				{error && <p style={{ color: "red" }}>{error}</p>}
 
-			{!isLoading && results && results.length === 0 && (
-				<p>
-					No games found for <em>"{lastQuery}"</em>. Please try a different
-					search query.
-				</p>
-			)}
+				{!isLoading && results && results.length === 0 && (
+					<p>
+						No games found for <em>"{lastQuery}"</em>. Please try a different
+						search query.
+					</p>
+				)}
+				{results && results.length > 0 && (
+					<Group>
+						<Select
+							label="Sort by"
+							placeholder="Choose sorting option"
+							data={[
+								{ value: "alphabetical", label: "Alphabetical" },
+								{ value: "lowestPrice", label: "Lowest Price" },
+								{ value: "highestDiscount", label: "Highest Discount" },
+							]}
+							value={sortOption}
+							onChange={handleSortChange}
+						/>
+					</Group>
+				)}
+				<Flex
+					mih={50}
+					gap="xl"
+					justify="flex-start"
+					align="center"
+					direction="row"
+					wrap="wrap"
+					mt="lg"
+				>
+					{results?.map((deal) => (
+						<DealCard key={deal.dealID} deal={deal} />
+					))}
+				</Flex>
 
-			{results && results.length > 0 && (
-				<Group>
-					<Select
-						label="Sort by"
-						placeholder="Choose sorting option"
-						data={[
-							{ value: "alphabetical", label: "Alphabetical" },
-							{ value: "lowestPrice", label: "Lowest Price" },
-							{ value: "highestDiscount", label: "Highest Discount" },
-						]}
-						value={sortOption}
-						onChange={handleSortChange}
-					/>
-				</Group>
-			)}
-
-			<Flex
-				mih={50}
-				gap="xl"
-				justify="flex-start"
-				align="center"
-				direction="row"
-				wrap="wrap"
-			>
-				{results?.map((deal) => (
-					<DealCard key={deal.dealID} deal={deal} />
-				))}
-			</Flex>
-
-			{results && results.length > 0 && (
-				<Group>
-					<Pagination
-						value={activePage}
-						onChange={setActivePage}
-						total={Math.ceil(totalResults / pageSize)}
-						mt="lg"
-					/>
-					<Select
-						label="Results per page"
-						placeholder="Choose page size"
-						data={[
-							{ value: "5", label: "5" },
-							{ value: "10", label: "10" },
-							{ value: "20", label: "20" },
-							{ value: "50", label: "50" },
-						]}
-						onChange={handlePageSizeChange}
-					/>
-				</Group>
-			)}
+				{results && results.length > 0 && (
+					<Group>
+						<Pagination
+							value={activePage}
+							onChange={setActivePage}
+							total={Math.ceil(totalResults / pageSize)}
+							mt="lg"
+						/>
+						<Select
+							label="Results per page"
+							placeholder="Choose page size"
+							data={[
+								{ value: "5", label: "5" },
+								{ value: "10", label: "10" },
+								{ value: "20", label: "20" },
+								{ value: "50", label: "50" },
+							]}
+							onChange={handlePageSizeChange}
+						/>
+					</Group>
+				)}
+			</div>
 		</>
 	);
 };
