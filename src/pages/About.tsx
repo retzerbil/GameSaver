6927b7435cfbb8aa56d41ea790a062290c5a5570
Andreas Retzius <andreasretzius@gmail.com>
2024-12-30 16:22:27 +0100

    updated about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,12 +3,12 @@ export const About = () => {
 		<>
 			<h1>About</h1>
 			<p>
-				This is a simple example of a React application with a few pages and
-				components. The app uses the
-				<a href="https://apidocs.cheapshark.com/">CheapShark API</a> to fetch
+				GameSaver is a web application that helps users find the best deals on video games.
+                The app uses the <a href="https://apidocs.cheapshark.com/"> <u>CheapShark API</u></a> to fetch
 				games and display them in a list. The user can search for games, sort
 				the results, and find the cheapest price for their favorite games. The
-				app is built with React, TypeScript, Mantine UI library and Tabler icons.
+				app is built with React, TypeScript, Mantine UI library and Tabler
+				icons.
 			</p>
 		</>
 	);
