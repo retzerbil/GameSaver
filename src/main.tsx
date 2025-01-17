067df698398d54cc8544fac61952aacf340ebf10
Andreas Retzius <andreasretzius@gmail.com>
2025-01-17 19:02:34 +0100

    Added Vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { MantineProvider } from '@mantine/core'
 import '@mantine/core/styles.css';
+import { Analytics } from '@vercel/analytics/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <MantineProvider>
      <App />
+     <Analytics />
     </MantineProvider>
   </React.StrictMode>,
 )
